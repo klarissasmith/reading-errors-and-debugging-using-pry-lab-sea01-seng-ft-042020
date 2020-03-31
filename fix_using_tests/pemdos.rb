620ddbee00eb0c9c77e00ed5188bdd37aca6517b
Klarissa Smith <ksmith_email@yahoo.com>
2020-03-31 21:00:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    new_string = 10.times * "s"
+    
   else
     string
   end
 end
+binding.pry
